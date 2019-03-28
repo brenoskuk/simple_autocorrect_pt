5d414ad462917a6ee5417e43b1c725cf62437545
Breno Skuk <meta.breno.skuk@serasa.intranet>
2019-03-28 18:21:18 -0300

    Added setup and readme

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simple_autocorrect_pt",
+    version="0.0.1",
+    author="Breno Skuk",
+    author_email="breno.skuk@gmail.com",
+    description="Autocorrect to be used in the pipeline of rasa that corrects PT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/brenoskuk/simple_autocorrect_pt",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: NONE",
+        "Operating System :: OS Independent",
+    ]
+)
